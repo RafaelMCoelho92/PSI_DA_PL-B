0f10d045cf61a46cb78bb16331e7065079ed1bb1
AndreDuarte95 <114529157+AndreDuarte95@users.noreply.github.com>
2023-05-18 23:38:29 +0100

    Update Bilhete.cs

--- app/Projeto_DA_PL_B_2223/Bilhete.cs
+++ app/Projeto_DA_PL_B_2223/Bilhete.cs
@@ -12,23 +12,23 @@ namespace Projeto_DA_PL_B_2223
         public double idBilhete { get; }
         public double lugarBilhete { get; }
         public bool estadoBilhete { get; set; }
-        public Filme filmeBilhete { get; }
+        //public Filme filmeBilhete { get; } -------> FILME e SALA vao estar na SESSAO
         public Cliente clienteBilhete { get;}
-        public Sala salaBilhete { get; }
+        //public Sala salaBilhete { get; }
         public Funcionario funcionarioBilhete { get; }
         //public Sessao sessaoBilhete { get; }
 
         //CRIADA ESTA VARIAVEL ESTATICA PARA SER INCREMENTADA SEMPRE QUE UM OBJ BILHETE SEJA CRIADO
         private static int contadorBilhete = 0;
 
-        public Bilhete(double idBilhete, double lugarBilhete, bool estadoBilhete, Filme filmeBilhete, Cliente clienteBilhete, Sala salaBilhete, Funcionario funcionarioBilhete)
+        public Bilhete(double idBilhete, double lugarBilhete, bool estadoBilhete, /*Filme filmeBilhete,*/ Cliente clienteBilhete, /*Sala salaBilhete,*/ Funcionario funcionarioBilhete)
         {
             this.idBilhete = idBilhete;
             this.lugarBilhete = lugarBilhete;
             this.estadoBilhete = estadoBilhete;
-            this.filmeBilhete = filmeBilhete;
+            //this.filmeBilhete = filmeBilhete;
             this.clienteBilhete = clienteBilhete;
-            this.salaBilhete = salaBilhete;
+            //this.salaBilhete = salaBilhete;
             this.funcionarioBilhete = funcionarioBilhete;
         }
 
